7d276324080977109ef5eb767cc4c12ece49f60f
cmr96 <carlos1m2r3@hotmail.com>
2015-11-05 13:31:22 +0100

    Primera

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CMR
+# Carlos Martinez Romero 
+# carlos1m2r3@hotmail.com
---
+++ ejemplo.php
@@ -0,0 +1,24 @@
+<html>
+
+<head></head>
+
+<body>
+    <?php
+      
+    //comentario
+
+    /* comentario 
+    multiple */
+
+    define('PI,3.14');
+    echo PI;
+    
+    $variable="hola";
+    echo $variable;    
+
+    
+
+    ?>    
+</body>
+
+</html>
